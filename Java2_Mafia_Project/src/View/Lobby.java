5019ab51d1d2a799966389b53f8e16c03eafea33
Shinyunha <82927944+Shinyunha@users.noreply.github.com>
2025-11-16 22:31:54 +0900

    닉네임 입력받아 ClientManager에게 보내기

--- Java2_Mafia_Project/src/View/Lobby.java
+++ Java2_Mafia_Project/src/View/Lobby.java
@@ -5,12 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import Client.ClientManager;
+
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import javax.swing.JList;
 import java.awt.Color;
+import javax.swing.JTextField;
 
 public class Lobby extends JFrame {
 
@@ -20,6 +27,8 @@ public class Lobby extends JFrame {
 	private JButton startButton;
 	private JLabel playerTextLabel;
 	private JList playerList;
+	private JTextField textField;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -51,24 +60,45 @@ public class Lobby extends JFrame {
 		titleTestLabel = new JLabel("마피아 게임 프로젝트");
 		titleTestLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		titleTestLabel.setFont(new Font("굴림", Font.PLAIN, 80));
-		titleTestLabel.setBounds(0, 43, 966, 148);
+		titleTestLabel.setBounds(0, 43, 966, 102);
 		contentPane.add(titleTestLabel);
 		
 		startButton = new JButton("시작하기");
 		startButton.setFont(new Font("굴림", Font.PLAIN, 50));
-		startButton.setBounds(312, 369, 334, 102);
+		startButton.setBounds(312, 395, 334, 102);
 		contentPane.add(startButton);
 		
 		playerTextLabel = new JLabel("입장한 플레이어");
 		playerTextLabel.setFont(new Font("굴림", Font.PLAIN, 30));
 		playerTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		playerTextLabel.setBounds(312, 160, 334, 49);
+		playerTextLabel.setBounds(312, 186, 334, 49);
 		contentPane.add(playerTextLabel);
 		
 		playerList = new JList();
 		playerList.setBackground(new Color(192, 192, 192));
-		playerList.setBounds(182, 211, 596, 135);
+		playerList.setBounds(182, 237, 596, 135);
 		contentPane.add(playerList);
+		
+		textField = new JTextField();
+		textField.setBounds(483, 155, 106, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JTextField nickname = (JTextField) e.getSource(); 				
+				ClientManager.sendMessage(nickname.getText()); 
+				System.out.println("닉네임 "+ nickname.getText() + " ClientManeger에게 전달완료");
+				nickname.setText("");
+			}
+		});
+		
+		lblNewLabel = new JLabel("닉네임을 입력하세요");
+		lblNewLabel.setBounds(359, 158, 112, 15);
+		contentPane.add(lblNewLabel);
 
 	}
 }
